ec7ae6e418e672e40747bddb44f9edf71b74e243
Kyaw Zin Htet <kyawzinhtet7930@gmail.com>
2022-12-18 17:49:54 +0630

    crypto scanner app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p className='text-red-700'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -61,4 +59,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
